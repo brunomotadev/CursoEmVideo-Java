59400c8fd85fcbeb468f8c62d284584080b0cfee
brunomotadev <brunomotadev@gmail.com>
2021-03-05 00:34:46 -0300

    Operadores Aritmeticos

---
+++ OperadoresAritmeticos/src/operadoresAritmeticos.java
@@ -0,0 +1,39 @@
+
+public class operadoresAritmeticos {
+
+	public static void main(String[] args) {
+		/*int n1 = 3;
+		int n2 = 5;
+		float m = (n1 + n2)/2;
+		
+		System.out.println("A média é igual a " + m);*/
+		/*
+		int numero = 5;
+		numero++;
+		System.out.println(numero);
+		numero--;
+		System.out.println(numero);
+		*/
+		/*
+		int numero = 10;
+		int valor = 4 + numero--;
+		System.out.println(valor);
+		System.out.println(numero);
+		*/
+		/*
+		int x = 4;
+		x *= 2;
+		System.out.println(x);
+		*/
+		
+		/*
+		float v = 8.5f;
+		int ar = (int) Math.ceil(v);
+		System.out.println(ar);
+		*/
+		
+		double ale = Math.random();
+		int n = (int) (15 + ale * (50-15));
+		System.out.println(n);
+	}
+}
