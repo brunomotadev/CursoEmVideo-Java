71569829709d2c87b01fc07f5f088541b232c1c5
brunomotadev <brunomotadev@gmail.com>
2021-03-06 10:15:50 -0300

    Revisão Vetores Java

---
+++ Vetores/src/vetor03/Vetor03.java
@@ -0,0 +1,15 @@
+package vetor03;
+
+public class Vetor03 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		double v[]= {3.5, 2.75, 9, -4.5};
+		
+		for(double valor: v) {
+			System.out.println(valor + " ");
+		}
+	}
+
+}
